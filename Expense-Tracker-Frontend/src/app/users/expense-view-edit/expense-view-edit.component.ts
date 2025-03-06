f78d425f45614a36f880297fbb25235114a98e12
Sayantan Singha <sayantan.singha@webskitters.in>
2025-03-06 15:14:32 +0530

    expense view edit toogle done

--- Expense-Tracker-Frontend/src/app/users/expense-view-edit/expense-view-edit.component.ts
+++ Expense-Tracker-Frontend/src/app/users/expense-view-edit/expense-view-edit.component.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, inject, ResourceRef, signal } from '@angular/core';
+import { DOCUMENT, NgStyle } from '@angular/common';
+import { Component, effect, inject, ResourceRef, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
@@ -7,11 +7,11 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { ActivatedRoute } from '@angular/router';
-import { AlertService, ApiService, EventService } from '@services';
+import { AlertService, ApiService } from '@services';
 
 @Component({
   selector: 'app-expense-view-edit',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatDatepickerModule, MatInput],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatDatepickerModule, MatInput, NgStyle],
   templateUrl: './expense-view-edit.component.html',
   styleUrl: './expense-view-edit.component.scss',
   providers: [provideNativeDateAdapter()],
@@ -20,7 +20,6 @@ export class ExpenseViewEditComponent {
   /* Injected Variables */
   private readonly api = inject(ApiService);
   private readonly alert = inject(AlertService);
-  private readonly event = inject(EventService);
   private readonly document = inject(DOCUMENT);
   private readonly activatedRoute = inject(ActivatedRoute);
 
@@ -32,7 +31,9 @@ export class ExpenseViewEditComponent {
   });
 
   /* Variables */
+  protected editing = signal<boolean>(false);
   protected expenseId = signal<string>('');
+  protected expense = signal<any>({});
   protected form: FormGroup = new FormGroup({
     amount: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/)]),
     description: new FormControl('', Validators.required),
@@ -47,6 +48,15 @@ export class ExpenseViewEditComponent {
       this.expenseId.set(params.get('id') || '');
       this.getCategoryDetails();
     });
+
+    effect(() => {
+      const editing = this.editing();
+      if (editing) {
+        this.form.enable();
+      } else {
+        this.form.disable();
+      }
+    });
   }
 
   private getCategoryDetails() {
@@ -54,7 +64,7 @@ export class ExpenseViewEditComponent {
     this.api.get(`api/expense/fetch-by-id/${id}`).subscribe({
       next: (response: any) => {
         const expense = response.data;
-        console.log("expense: ", expense);
+        this.expense.set(response.data);
         this.form.patchValue({
           categoryId: expense?.category?._id,
           description: expense?.description || '',
