388030e1a913c26808f00ed3056cd4b681fa3553
Sayantan Singha <sayantan.singha@webskitters.in>
2025-03-07 19:48:09 +0530

    statement modal done

--- Expense-Tracker-Frontend/src/app/modals/statement-view-export/statement-view-export.component.ts
+++ Expense-Tracker-Frontend/src/app/modals/statement-view-export/statement-view-export.component.ts
@@ -5,7 +5,7 @@ import { MatIcon } from '@angular/material/icon';
 import { AlertService, ApiService } from '@services';
 import { Subscription } from 'rxjs';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerModule, MatDateRangePicker } from '@angular/material/datepicker';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButton } from '@angular/material/button';
@@ -28,42 +28,35 @@ export class StatementViewExportDialog {
   private readonly destroyRef = inject(DestroyRef);
 
   /* Variables */
-  readonly byDuration: string[] = ['Last Week', 'Last Month', 'Custom Date Range'];
   protected step = signal<1 | 2>(1);
   protected filterForm: FormGroup = new FormGroup({
     limit: new FormControl(0),
+    _limit: new FormControl(''), // to not show 0
     page: new FormControl(1),
-    startDate: new FormControl(""),
-    endDate: new FormControl(""),
-    type: new FormControl(""),
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    type: new FormControl<'' | 'cash-in' | 'cash-out'>(""),
     pagination: new FormControl(false),
+    byDuration: new FormControl<'last-week' | 'last-month' | 'custom-date-range' | ''>(''),
   });
   private filterSubscriber!: Subscription;
   protected expenses = signal<Array<any>>([]);
 
   constructor() {
-    console.log("data: ", this.DIALOG_DATA);
-
     this.filterSubscriber = this.filterForm.valueChanges.subscribe({
       next: (value: any) => {
         console.log("value: ", value);
-        this.getExpenses(value);
       }, error: (err: any) => {
         console.log("err: ", err);
       }
     });
-
-    setTimeout(() => {
-      this.filterForm.controls['limit'].patchValue(5);
-    }, 3000);
-
 
     this.destroyRef.onDestroy(() => {
       this.filterSubscriber.unsubscribe();
     });
   }
 
-  private getExpenses(filter: any) {
+  protected getExpenses(filter: any) {
     let url: string = `api/expense/fetch-by-filter-with-report?limit=${filter?.limit || 10}&page=${filter?.page || 1}&pagination=${!!filter?.pagination}`;
     ((filter.startDate && filter.startDate.length) && (filter.endDate && filter.endDate.length)) && (url += `&startDate=${filter.startDate}&endDate=${filter.endDate}`);
     (filter.type && filter.type.length) && (url += '&type=' + filter.type);
@@ -77,6 +70,71 @@ export class StatementViewExportDialog {
         console.log("error: ", error);
       }
     });
+  }
+
+  formValueChange(controlName: '_limit' | 'byDuration') {
+    setTimeout(() => {
+      const value = this.filterForm.controls[controlName].value;
+
+      switch (controlName) {
+        case '_limit':
+          if (value && value.toString().length) { // _limit>0  then
+            this.filterForm.controls['startDate'].patchValue('');
+            this.filterForm.controls['endDate'].patchValue('');
+            this.filterForm.controls['byDuration'].patchValue('');
+            this.filterForm.controls['pagination'].patchValue(true);
+            this.filterForm.controls['limit'].patchValue(value);
+          } else {
+            this.filterForm.controls['pagination'].patchValue(false);
+          }
+          break;
+        case 'byDuration':
+          if (value && value.length) {
+            this.filterForm.controls['limit'].patchValue(0);
+            this.filterForm.controls['_limit'].patchValue('');
+
+            if (value == 'last-week' || value == 'last-month') {
+              if (value == 'last-week') {
+                // Get today's date and set time to 23:59:59
+                const currentDate = new Date();
+                currentDate.setHours(23, 59, 59, 999); // Set time to 23:59:59.999
+
+                // Get the date 7 days ago and set time to 00:00:00
+                const sevenDaysAgo = new Date();
+                sevenDaysAgo.setDate(currentDate.getDate() - 6);
+                sevenDaysAgo.setHours(0, 0, 0, 0); // Set time to 00:00:00.000
+
+                // Patch the values into the form controls
+                this.filterForm.controls['startDate'].patchValue(sevenDaysAgo);
+                this.filterForm.controls['endDate'].patchValue(currentDate);
+              } else {
+                // Get today's date and set time to 23:59:59
+                const currentDate = new Date();
+                currentDate.setHours(23, 59, 59, 999); // Set time to 23:59:59.999
+
+                // Get the date 30 days ago and set time to 00:00:00
+                const thirtyDaysAgo = new Date();
+                thirtyDaysAgo.setDate(currentDate.getDate() - 29);
+                thirtyDaysAgo.setHours(0, 0, 0, 0); // Set time to 00:00:00.000
+
+                // Patch the values into the form controls
+                this.filterForm.controls['startDate'].patchValue(thirtyDaysAgo);
+                this.filterForm.controls['endDate'].patchValue(currentDate);
+              }
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    }, 50);
+  }
+
+  protected rangePickerClickedFromInput(rangePicker: MatDateRangePicker<any>) {
+    const byDurationValue = this.filterForm.controls['byDuration'].value;
+    if (byDurationValue == 'custom-date-range') {
+      rangePicker.open();
+    }
   }
 
   public exportStatement(): void { }
