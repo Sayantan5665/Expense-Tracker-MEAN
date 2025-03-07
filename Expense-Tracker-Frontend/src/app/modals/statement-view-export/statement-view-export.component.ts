3964f57f8d8a0ac0e2c43740b690d2cfc535535b
Sayantan Singha <sayantan.singha@webskitters.in>
2025-03-07 17:01:41 +0530

    statement work 80% done

--- Expense-Tracker-Frontend/src/app/modals/statement-view-export/statement-view-export.component.ts
+++ Expense-Tracker-Frontend/src/app/modals/statement-view-export/statement-view-export.component.ts
@@ -1,14 +1,23 @@
-import { Component, effect, inject, ResourceRef, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { AlertService, ApiService } from '@services';
+import { Subscription } from 'rxjs';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe, DatePipe, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-statement-view-export',
-  imports: [MatIcon, MatDialogModule],
+  imports: [MatIcon, MatDialogModule, ReactiveFormsModule, MatFormFieldModule, MatChipsModule,
+    MatDatepickerModule, MatNativeDateModule, DatePipe, MatButton, CurrencyPipe, NgStyle],
   templateUrl: './statement-view-export.component.html',
-  styleUrl: './statement-view-export.component.scss'
+  styleUrl: './statement-view-export.component.scss',
+  providers: [provideNativeDateAdapter()]
 })
 export class StatementViewExportDialog {
   /* Dependency Injection */
@@ -16,32 +25,57 @@ export class StatementViewExportDialog {
   protected DIALOG_DATA: any = inject(MAT_DIALOG_DATA);
   private readonly api = inject(ApiService);
   private readonly alert = inject(AlertService);
+  private readonly destroyRef = inject(DestroyRef);
 
   /* Variables */
+  readonly byDuration: string[] = ['Last Week', 'Last Month', 'Custom Date Range'];
   protected step = signal<1 | 2>(1);
-  protected filterOption = signal({
-    startDate: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
-    endDate: new Date()
+  protected filterForm: FormGroup = new FormGroup({
+    limit: new FormControl(0),
+    page: new FormControl(1),
+    startDate: new FormControl(""),
+    endDate: new FormControl(""),
+    type: new FormControl(""),
+    pagination: new FormControl(false),
   });
-
-  /* Api Calls */
-  protected expenses: ResourceRef<any> = rxResource({
-    request: () => ({ filterOption: this.filterOption() }),
-    loader: (e) => {
-      const filter = e.request.filterOption;
-      return this.api.get(`api/expense/fetch-by-filter-with-report?startDate=${filter.startDate}&endDate=${filter.endDate}`);
-    },
-  });
+  private filterSubscriber!: Subscription;
+  protected expenses = signal<Array<any>>([]);
 
   constructor() {
     console.log("data: ", this.DIALOG_DATA);
 
-    effect(() => {
-      const error:any = this.expenses.error();
-      if (error) {
-        this.alert.toast(error?.message || "Failed to fetch expenses", 'error');
+    this.filterSubscriber = this.filterForm.valueChanges.subscribe({
+      next: (value: any) => {
+        console.log("value: ", value);
+        this.getExpenses(value);
+      }, error: (err: any) => {
+        console.log("err: ", err);
       }
-      console.log("expenses: ", this.expenses.value());
+    });
+
+    setTimeout(() => {
+      this.filterForm.controls['limit'].patchValue(5);
+    }, 3000);
+
+
+    this.destroyRef.onDestroy(() => {
+      this.filterSubscriber.unsubscribe();
+    });
+  }
+
+  private getExpenses(filter: any) {
+    let url: string = `api/expense/fetch-by-filter-with-report?limit=${filter?.limit || 10}&page=${filter?.page || 1}&pagination=${!!filter?.pagination}`;
+    ((filter.startDate && filter.startDate.length) && (filter.endDate && filter.endDate.length)) && (url += `&startDate=${filter.startDate}&endDate=${filter.endDate}`);
+    (filter.type && filter.type.length) && (url += '&type=' + filter.type);
+
+    this.api.get(url).subscribe({
+      next: (res: any) => {
+        console.log("res?.data?.docs: ", res?.data?.docs);
+        this.expenses.set(res?.data?.docs || []);
+      },
+      error: (error: any) => {
+        console.log("error: ", error);
+      }
     });
   }
 
