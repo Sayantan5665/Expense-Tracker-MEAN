0cff471a27ed596885ddd5a45fd580efdf06ae49
Sayantan Singha <sayantan.singha@webskitters.in>
2025-03-10 20:34:52 +0530

    CMS pages

--- Expense-Tracker-Frontend/src/app/pages/privacy-policy/privacy-policy.component.ts
+++ Expense-Tracker-Frontend/src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-
+  protected today = new Date();
 }
--- Expense-Tracker-Frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ Expense-Tracker-Frontend/src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,11 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terms-and-conditions',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './terms-and-conditions.component.html',
   styleUrl: './terms-and-conditions.component.scss'
 })
 export class TermsAndConditionsComponent {
-
+  protected today = new Date();
 }
